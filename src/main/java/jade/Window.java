257a1941f93c40de8279b1333df33b1f7e4677d2
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-06-28 14:24:01 -0600

    Reformat code style

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -23,76 +23,76 @@ public class Window {
     }
 
     public static Window get() {
-        if ( Window.window == null ) {
-            Window.window = new Window ();
+        if(Window.window == null) {
+            Window.window = new Window();
         }
         return Window.window;
     }
 
     public void run() {
-        System.out.println ("Hello LWJGL " + Version.getVersion () + "!");
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
 
         init();
         loop();
 
         // Free the memory
-        glfwFreeCallbacks (glfwWindow);
-        glfwDestroyWindow (glfwWindow);
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
 
         // Terminate GLFW and free the error callback
-        glfwTerminate ();
-        glfwSetErrorCallback (null).free ();
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
     }
 
     public void init() {
         // Setup an error callback
-        GLFWErrorCallback.createPrint (System.err).set ();
+        GLFWErrorCallback.createPrint(System.err).set();
 
         // Initialize GLFW
-        if ( !glfwInit() ) {
-            throw new IllegalStateException ("Unable to initialize GLFW");
+        if(! glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
         }
 
         // Configure GLFW
-        glfwDefaultWindowHints ();
-        glfwWindowHint (GLFW_VISIBLE, GLFW_FALSE);
-        glfwWindowHint (GLFW_RESIZABLE, GLFW_TRUE);
-        glfwWindowHint (GLFW_MAXIMIZED, GLFW_TRUE);
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
 
         // Create the window
-        glfwWindow = glfwCreateWindow (width, height, title, NULL, NULL);
-        if ( glfwWindow == NULL ) {
-            throw new IllegalStateException ("Failed to create the GLFW window.");
+        glfwWindow = glfwCreateWindow(width, height, title, NULL, NULL);
+        if(glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create the GLFW window.");
         }
 
         // Make the OpenGL context current
-        glfwMakeContextCurrent (glfwWindow);
+        glfwMakeContextCurrent(glfwWindow);
 
         // Enable v-sync
-        glfwSwapInterval (1);
+        glfwSwapInterval(1);
 
         // Make the window visible
-        glfwShowWindow (glfwWindow);
+        glfwShowWindow(glfwWindow);
 
         // This line is critical for LWJGL's interoperation with GLFW's
         // OpenGL context, or any context that is managed externally.
         // LWJGL detects the context that is current in the current thread,
         // creates the GLCapabilities instance and makes the OpenGL
         // bindings available for use.
-        GL.createCapabilities ();
+        GL.createCapabilities();
     }
 
     public void loop() {
-        while (!glfwWindowShouldClose (glfwWindow)) {
+        while(! glfwWindowShouldClose(glfwWindow)) {
             // Poll events
-            glfwPollEvents ();
+            glfwPollEvents();
 
             // Set the clear color
-            glClearColor (1.0f, 1.0f, 1.0f, 1.0f);
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
             // Clear the framebuffer
-            glClear (GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT);
 
-            glfwSwapBuffers (glfwWindow);
+            glfwSwapBuffers(glfwWindow);
         }
     }
 }
