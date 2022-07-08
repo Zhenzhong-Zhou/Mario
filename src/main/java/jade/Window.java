b0859cac1e6963d9bb229c2b92401fc266ca6ba4
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-07-08 14:38:14 -0600

    Drawing the first square and add new config for macOS>=10.17

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -35,10 +35,11 @@ public class Window {
         switch(newScene) {
             case 0:
                 currentScene = new LevelEditorScene();
-//                currentScene.init();
+                currentScene.init();
                 break;
             case 1:
                 currentScene = new LevelScene();
+                currentScene.init();
                 break;
             default:
                 assert false: "Unknown scene '" + newScene + "'";
@@ -82,6 +83,9 @@ public class Window {
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
         glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
         // Create the window
         glfwWindow = glfwCreateWindow(width, height, title, NULL, NULL);
