38e6f695ce55650fa45469c57baf180eda745d32
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-07-10 16:05:08 -0600

    Fix bug

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -3,7 +3,6 @@ package jade;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
-import utilities.Time;
 
 import java.util.Objects;
 
@@ -126,7 +125,7 @@ public class Window {
     }
 
     public void loop() {
-        float beginTime = Time.getTime();
+        float beginTime = (float) glfwGetTime();
         float endTime;
         float dt = -1.0f;
 
@@ -145,7 +144,7 @@ public class Window {
 
             glfwSwapBuffers(glfwWindow);
 
-            endTime = Time.getTime();
+            endTime = (float) glfwGetTime();
             dt = endTime - beginTime;
             beginTime = endTime;
         }
