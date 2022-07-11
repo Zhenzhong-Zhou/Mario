8fb7326f42f9b59c18c0226e4e43101894ecfb41
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-07-11 17:18:45 -0600

    Add integrating ImGui

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -12,9 +12,11 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Window {
-    private final int width, height;
+    private int width, height;
     private final String title;
     private long glfwWindow;
+    private ImGuiLayer imGuiLayer;
+
     public float r, g, b, a;
 
     private static Window window = null;
@@ -105,6 +107,10 @@ public class Window {
         glfwSetMouseButtonCallback(glfwWindow, MouseListener :: mouseButtonCallback);
         glfwSetScrollCallback(glfwWindow, MouseListener :: mouseScrollCallback);
         glfwSetKeyCallback(glfwWindow, KeyListener :: keyCallback);
+        glfwSetWindowSizeCallback(glfwWindow, (w, newWidth, newHeight) -> {
+            Window.setWidth(newWidth);
+            Window.setHeight(newHeight);
+        });
 
         // Make the OpenGL context current
         glfwMakeContextCurrent(glfwWindow);
@@ -124,6 +130,8 @@ public class Window {
 
         glEnable(GL_BLEND);
         glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+        this.imGuiLayer = new ImGuiLayer(glfwWindow);
+        this.imGuiLayer.initImGui();
 
         Window.changeScene(0);
     }
@@ -146,11 +154,28 @@ public class Window {
                 currentScene.update(dt);
             }
 
+            this.imGuiLayer.update(dt);
             glfwSwapBuffers(glfwWindow);
 
             endTime = (float) glfwGetTime();
             dt = endTime - beginTime;
             beginTime = endTime;
         }
+    }
+
+    public static int getWidth() {
+        return get().width;
+    }
+
+    public static int getHeight() {
+        return get().height;
+    }
+
+    public static void setWidth(int newWidth) {
+        get().width = newWidth;
+    }
+
+    public static void setHeight(int newHeight) {
+        get().height = newHeight;
     }
 }
