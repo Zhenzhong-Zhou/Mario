03ea8672321e4cb8e9a7f130b013ec4eab614ffa
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-07-13 15:07:24 -0600

    Minor changes

--- src/main/java/jade/Camera.java
+++ src/main/java/jade/Camera.java
@@ -6,7 +6,7 @@ import org.joml.Vector3f;
 
 public class Camera {
     private Matrix4f projectionMatrix, viewMatrix, inverseProjection, inverseView;
-    public final Vector2f position;
+    public Vector2f position;
 
     public Camera(Vector2f position) {
         this.position = position;
@@ -26,14 +26,14 @@ public class Camera {
     public Matrix4f getViewMatrix() {
         Vector3f cameraFront = new Vector3f(0.0f, 0.0f, -1.0f);
         Vector3f cameraUp = new Vector3f(0.0f, 1.0f, 0.0f);
-        viewMatrix.identity();
+        this.viewMatrix.identity();
         viewMatrix.lookAt(new Vector3f(position.x, position.y, 20.0f), cameraFront.add(position.x, position.y, 0.0f), cameraUp);
         this.viewMatrix.invert(inverseView);
-        return viewMatrix;
+        return this.viewMatrix;
     }
 
     public Matrix4f getProjectionMatrix() {
-        return projectionMatrix;
+        return this.projectionMatrix;
     }
 
     public Matrix4f getInverseProjection() {
