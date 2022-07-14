ded3fc54b45a73d8ffa769f40f0078f69268a912
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-07-13 20:14:23 -0600

    Add debug drawing functionality

--- src/main/java/scenes/LevelEditorScene.java
+++ src/main/java/scenes/LevelEditorScene.java
@@ -5,7 +5,9 @@ import imgui.ImGui;
 import imgui.ImVec2;
 import jade.*;
 import org.joml.Vector2f;
+import org.joml.Vector3f;
 import org.joml.Vector4f;
+import renderer.DebugDraw;
 import utilities.AssetPool;
 
 public class LevelEditorScene extends Scene {
@@ -54,9 +56,15 @@ public class LevelEditorScene extends Scene {
         AssetPool.getTexture("assets/images/blendGreen.png");
     }
 
+    float t = 0.0f;
     @Override
     public void update(float dt) {
         mouseControls.update(dt);
+
+        float x = ((float) Math.sin(t)  * 200.0f) + 600;
+        float y = ((float) Math.cos(t)  * 200.0f) + 400;
+        t += 0.5f;
+        DebugDraw.addLine2D(new Vector2f(600, 400), new Vector2f(x, y), new Vector3f(0, 0, 1), 10);
 
         for(GameObject gameObject : gameObjects) {
             gameObject.update(dt);
